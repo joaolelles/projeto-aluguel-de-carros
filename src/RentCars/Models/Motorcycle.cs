b352d2acfbaeac88bb6690d2f3a2322422af8009
João <joaolelles7@gmail.com>
2023-07-17 16:42:42 -0300

    Requisito 7, Feat: Criando a classe Motorcycle

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -5,4 +5,7 @@ namespace RentCars.Models;
 //7 - Faça a classe `Motorcycle` herdar de `Vehicle`
 public class Motorcycle : Vehicle
 {
+    public int SeatHeight { get; set; }
+    public BrakeType FrontBrake { get; set; }
+    public BrakeType RearBrake { get; set; }
 }
